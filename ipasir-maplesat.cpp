6f27586e72dfa31b26f754ff7743bebad8265689
Vedad Hadzic <vedad.hadzic@student.tugraz.at>
2018-08-06 08:41:55 +0200

    Trying something with phase saving

--- ipasir-maplesat.cpp
+++ ipasir-maplesat.cpp
@@ -75,6 +75,7 @@ public:
     // MapleSAT by default produces non standard conforming messages.
     // So either we have to set this to '0' or patch the sources.
     verbosity = 0;
+    phase_saving = 2;
   }
   
   ~IPAsirMapleSAT()
